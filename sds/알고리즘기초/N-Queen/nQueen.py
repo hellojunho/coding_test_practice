d9e88a19f1b12c67e16e904becf26033f184150f
hellojunho <jo62698476@gmail.com>
2023-02-15 01:00:15 +0900

    second commit

---
+++ sds/알고리즘기초/N-Queen/nQueen.py
@@ -0,0 +1,28 @@
+def check(x):
+    for i in range(x):
+        if arr[x] == arr[i]:
+            return False
+        elif arr[x] == arr[i] or abs(arr[x] - arr[i]) == x - i:
+            return False
+    return True
+
+
+def back_tracking(x):
+    global result
+    if x == n:
+        result += 1
+        print(arr, end='\n')
+
+    else:
+        for i in range(n):
+            arr[x] = i
+            if check(x):
+                back_tracking(x + 1)
+
+
+n = int(input())
+arr = [0] * n
+result = 0
+
+back_tracking(0)
+print(result)
