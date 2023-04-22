cf21f48b032b1c6f38801104146c54a73c9cb8c6
hellojunho <jo62698476@gmail.com>
2023-04-22 22:56:13 +0900

    add : 단계별 문제/재귀/재귀의 귀재/ror.py

---
+++ 단계별 문제/14. 재귀/재귀의 귀재/ror.py
@@ -0,0 +1,18 @@
+import sys
+input = sys.stdin.readline
+
+def recursion(s, l, r):
+    global count # 함수 내에서 전역 변수로 cnt를 활용하기 위해 global로 명시해준다.
+    count += 1
+    
+    if l >= r: return 1
+    elif s[l] != s[r]: return 0
+    else: return recursion(s, l+1, r-1)
+
+def isPalindrome(s):
+    return recursion(s, 0, len(s)-1)
+
+n = int(input())
+for i in range(n):
+    count = 0
+    print(isPalindrome(input().rstrip()), count)
